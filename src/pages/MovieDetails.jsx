815ef6ddb14daf70fdaaa43ce4224627f82b0c13
panchoalenda <pancho_alenda@yahoo.com.ar>
2024-02-26 18:45:54 -0300

    Subiendo Cambios detailMovie

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,8 +1,29 @@
+import { useParams } from "react-router-dom";
+import { Get } from "../data/HttpClient";
+import { useEffect, useState } from "react";
+
+
 export const MovieDetails = () => {
+  let { movieId } = useParams();
+  const [detail, setDetail] = useState(null);
+
+  useEffect( () => {
+     Get("/character/" + movieId).then((data) => {
+      setDetail(data);
+     console.log(data.id);
+    });
+  }, [movieId]);
+//console.log(detail.id)
   return (
-  <>
-  <h1>MovieDetails</h1>
-  
-  </>
-   )
+    <>
+      <h1>MovieDetails</h1>
+      
+      {/* <p>{detail.id}</p>
+      <p>{detail.gender}</p>
+      <p>{detail.species}</p>
+      <p>{detail.status}</p>
+      <p>{detail.url}</p>     */}
+
+    </>
+  )
 }
